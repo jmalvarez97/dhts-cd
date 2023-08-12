e3988a3274b31b337e52a7041ae1e0fc8127d975
jmalvarez97 <jualvarez@fi.uba.ar>
2023-08-12 10:44:55 -0300

    add post

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,39 @@
 import bodyParser from "body-parser";
 import express from "express";
 import pg from "pg";
+const cors = require('cors')
 
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
 const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
+
 
 app.get("/", async (req, res) => {
   const { rows } = await pool.query("SELECT NOW()");
   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
 });
+
+app.post("/", async (req, res) => {
+  const dataFromBody = req.body;
+
+  const query = 'INSERT INTO mediciones (s1h, s1t, s2h, s2t, date) VALUES ($1, $2, $3, $4, $5)';
+  const values = [dataFromBody.s1h, dataFromBody.s1t, dataFromBody.s2h, dataFromBody.s2t, Date.now()];
+
+  try {
+    const result = await pool.query(query, values);
+    console.log('Fila agregada correctamente');
+    res.status(200).send('Fila agregada correctamente');
+  } catch (err) {
+    console.error('Error al ejecutar la consulta:', err);
+    res.status(500).send('Error al agregar la fila en la base de datos');
+  }
+} ) 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
